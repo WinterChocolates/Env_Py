3e411da29d13028d5116b841a5ee663a362d593d
WinterChocolates <1728841959@qq.com>
2024-08-22 21:49:44 +0800

    咕咕咕

--- main.py
+++ main.py
@@ -4,12 +4,7 @@ import requests
 # 主目录获取
 home = os.environ.get('HOME')
 
-'''
-
-
-'''
-
-def net_status_code(url):
+def net_status_code(url: str) -> bool:
     try:
         r = requests.get(url, timeout=5)
         if r.status_code == 200:
@@ -18,24 +13,20 @@ def net_status_code(url):
             return r.status_code
     except requests.exceptions.Timeout:
         return False
-    except requests.exceptions.RequestException:
+    except requests.exceptions.ConnectionError:
         return False
-        
-
 
 def logo():
     print("1.安装nvm 2.卸载nvm")
 
 def nvm():
-    nvm_mirror='export NVM_NODEJS_ORG_MIRROR=https://npmmirror.com/mirrors/node/'
+    nvm_mirror = 'export NVM_NODEJS_ORG_MIRROR=https://npmmirror.com/mirrors/node/'
     os.system("wget -qO- https://mirror.ghproxy.com/https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | sed 's|https://raw.githubusercontent.com/|https://mirror.ghproxy.com/https://raw.githubusercontent.com/|g' | bash")
+    print(net_status_code("https://nodejs.org/"))
     if net_status_code("https://nodejs.org/") != 200:
-        with open (f'{home}/.bashrc','w') as f:
+        with open(f'{home}/.bashrc', 'w', encoding='utf-8') as f:
             f.write(nvm_mirror)
     print("环境变量已添加到当前用户目录下的./bashrc. 请运行 'source ~/.bashrc' 使更改生效。")
-
-
-
 
 
 if __name__ == "__main__":
@@ -43,7 +34,7 @@ if __name__ == "__main__":
     # if 'wget' not in result.read():
     #     print('正在安装相关依赖包: wget')
     #     os.system('dnf install screenfetch -y')
-    
+
     # option = int(input("选择的操作："))
     # if option == 1:
     #     nvm_dir=f'{home}/.nvm/'
@@ -52,6 +43,4 @@ if __name__ == "__main__":
     #         print("环境变量已添加到当前用户目录下的./bashrc. 请运行 'source ~/.bashrc' 使更改生效。")
     #     else:
     #         nvm()
-    # ping_www("https://nodejs.org/")
-    r = net_status_code("https://nodejs.org/")
-    print(r)
+    status = net_status_code("https://nodejs.org")
