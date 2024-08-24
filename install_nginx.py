ab3e51e9024409ba35587642c000e4202c326dd4
WinterChocolates <1728841959@qq.com>
2024-08-24 09:58:46 +0800

    二次优化

--- install_nginx.py
+++ install_nginx.py
@@ -20,20 +20,19 @@ def download_nginx(base_urls: list, nginx_version: str, download_dir: str):
         source_file = os.path.join(download_dir, f"nginx-{nginx_version}.tar.gz")
 
         print(f"正在尝试从 {nginx_url} 下载")
-        print(f"{source_file} 中...")
-        print("")
+        print(f"到 {source_file} 中...", end="\n")
         response = requests.get(nginx_url, timeout=5)
 
         if response.status_code == 200:
             with open(source_file, 'wb') as file:
                 file.write(response.content)
-            print(f"Nginx {nginx_version}下载完成。保存到{source_file}")
+            print(f"Nginx {nginx_version}下载完成。保存到{source_file}", end="\n")
             return source_file
         else:
             print(f"从{nginx_url}(HTTP{response.status_code})下载失败...")
-            print("正在尝试下一个URL...")
+            print("正在尝试下一个URL...", end="\n")
 
-    raise Exception(f"从所有提供的url下载Nginx {nginx_version}失败。")
+    raise Exception(f"从所有提供的url下载Nginx {nginx_version}失败。", end="\n")
 
 
 def disposition_nginx():
@@ -42,16 +41,16 @@ def disposition_nginx():
         print(f"解压{source_file}...")
         with tarfile.open(source_file, 'r:gz') as tar:
             tar.extractall(path=download_Dir)
-        print(f"Nginx{NGINX_VERSION}解压完成.")
+        print(f"Nginx{NGINX_VERSION}解压完成.", end="\n")
 
         print("安装开发工具包中...")
         os.system('dnf group install "Development Tools" -y')
-        print("开发工具包安装完成.")
+        print("开发工具包安装完成.", end="\n")
 
         print("安装Nginx必要库中...")
         os.system(
             "dnf install openssl openssl-devel pcre pcre-devel zlib zlib-devel -y")
-        print("必要库安装完成.")
+        print("必要库安装完成.", end="\n")
 
         source_dir = os.path.join(download_Dir, f"nginx-{NGINX_VERSION}")
         os.chdir(source_dir)
