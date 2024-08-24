555f44428fc93074d9054f22f602246b55c4ef09
WinterChocolates <1728841959@qq.com>
2024-08-24 19:50:52 +0800

    修改读取

--- install_nginx.py
+++ install_nginx.py
@@ -7,14 +7,18 @@ download_dir = os.path.expanduser("~/downloads")
 if not os.path.exists(download_dir):
     os.makedirs(download_dir)
 
-conf = read_yaml_file()
+config = read_yaml_file()
+app = config.get("nginx")
+base_urls = app.get("url")
+nginx_version = app.get("version")
+nginx_bin = app.get("bin")
 
 
-def download_nginx(conf: dict):
-    for base_url in conf.nginx.url:
-        nginx_url = f"{base_url}nginx-{conf.nginx.version}.tar.gz"
+def download_nginx(base_urls: list, nginx_version: str):
+    for base_url in base_urls:
+        nginx_url = f"{base_url}nginx-{nginx_version}.tar.gz"
         source_file = os.path.join(download_dir,
-                                   f"nginx-{conf.nginx.version}.tar.gz")
+                                   f"nginx-{nginx_version}.tar.gz")
 
         print(f"正在尝试从 {nginx_url} 下载")
         print(f"到 {source_file} 中...", end="\n")
@@ -23,22 +27,22 @@ def download_nginx(conf: dict):
         if response.status_code == 200:
             with open(source_file, 'wb') as file:
                 file.write(response.content)
-            print(f"Nginx {conf.nginx.version}下载完成。保存到{source_file}", end="\n")
+            print(f"Nginx {nginx_version}下载完成。保存到{source_file}", end="\n")
             return source_file
         else:
             print(f"从{nginx_url}(HTTP{response.status_code})下载失败...")
             print("正在尝试下一个URL...", end="\n")
 
-    raise Exception(f"从所有提供的url下载Nginx {conf.nginx.version}失败。", end="\n")
+    raise Exception(f"从所有提供的url下载Nginx {nginx_version}失败。", end="\n")
 
 
-def disposition_nginx(conf: dict):
+def disposition_nginx():
     try:
-        source_file = download_nginx(conf)
+        source_file = download_nginx(base_urls, nginx_version)
         print(f"解压{source_file}...")
         with tarfile.open(source_file, 'r:gz') as tar:
             tar.extractall(path=download_dir)
-        print(f"Nginx{conf.nginx.version}解压完成.", end="\n")
+        print(f"Nginx{nginx_version}解压完成.", end="\n")
 
         print("安装开发工具包中...")
         os.system('dnf group install "Development Tools" -y')
@@ -50,24 +54,23 @@ def disposition_nginx(conf: dict):
         )
         print("必要库安装完成.", end="\n")
 
-        source_dir = os.path.join(download_dir, f"nginx-{conf.nginx.version}")
+        source_dir = os.path.join(download_dir, f"nginx-{nginx_version}")
         os.chdir(source_dir)
-        print(f"在{os.getcwd()}中配置Nginx {conf.nginx.version}...")
-        os.system(f"./configure --prefix={conf.nginx.bin} --with-http_ssl_module --with-http_gzip_static_module --with-http_stub_status_module --with-pcre")
+        print(f"在{os.getcwd()}中配置Nginx {nginx_version}...")
+        os.system(f"./configure --prefix={nginx_bin} --with-http_ssl_module --with-http_gzip_static_module --with-http_stub_status_module --with-pcre")
 
-        print(f"编译Nginx {conf.nginx.version}")
+        print(f"编译Nginx {nginx_version}")
         os.system("make")
 
-        print(f"安装Nginx {conf.nginx.version}")
+        print(f"安装Nginx {nginx_version}")
         os.system("make install")
         os.chdir("..")
     except Exception as e:
         return e
 
-    print(f"完成Nginx {conf.nginx.version}的安装.")
-    print(f"安装目录为：{conf.nginx.bin}")
+    print(f"完成Nginx {nginx_version}的安装.")
+    print(f"安装目录为：{nginx_bin}")
 
 
 if __name__ == "__main__":
-    disposition_nginx(conf)
-    # print(conf)
+    disposition_nginx()
