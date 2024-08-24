4b68c5bef6b595a0b788f21b34126f62e0bca2d6
WinterChocolates <1728841959@qq.com>
2024-08-24 19:29:32 +0800

    新增yaml读取

---
+++ read_yaml.py
@@ -0,0 +1,16 @@
+import yaml
+
+
+def read_yaml_file():
+    file_yaml = 'conf.yaml'
+    with open(file_yaml, 'r', encoding="utf-8") as file:
+        try:
+            data = yaml.safe_load(file)
+            return data
+        except yaml.YAMLError as e:
+            print(f'读取{file_yaml}文件失败：{e}')
+            return None
+
+
+if __name__ == "__main__":
+    print(read_yaml_file())
